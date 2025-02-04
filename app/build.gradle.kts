6cf6ef3a3eb446aa3843e1092199b0602b25eefb
unknown <25666053+ployt0@users.noreply.github.com>
2025-02-04 15:59:29 +0000

    review and use toml file for versions. modernise all versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,11 +53,11 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.json)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    testImplementation("org.json:json:20250107")
 }
